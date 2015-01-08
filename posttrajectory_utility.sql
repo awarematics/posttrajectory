0c14ab2780ef7167338160b04af28150ace23619
manner7979 <manner7979@gmail.com>
2015-01-09 03:19:09 +0900

    update utility

---
+++ posttrajectory_utility.sql
@@ -0,0 +1,30 @@
+﻿
+
+-- FUNCTION DEFINITION
+-- DESCRIPTION : 
+-- NAME : TT_PERIOD(TIMESTAMP, TIMESTAMP)
+-- RETURNS : TIMESTAMP, TIMESTAMP -> PERIODS
+-- CREATED BY YOO KI HYUN
+
+-- START TT_PERIOD:
+
+-- DROP FUNCTION TT_PERIOD(TIMESTAMP, TIMESTAMP);
+
+CREATE OR REPLACE FUNCTION TT_PERIOD(TIMESTAMP, TIMESTAMP) RETURNS periods AS 
+$$
+DECLARE
+	startTime		alias for $1;
+	endTime			alias for $2;
+	result_periods		periods;
+	
+BEGIN
+	result_periods.startTime := startTime;
+	result_periods.endTime := endTime;	
+
+	RETURN result_periods;
+END
+$$
+LANGUAGE 'plpgsql';
+
+-- END TT_PERIOD:
+------------------------------------------------------------------------------------------
