7ff9fb4b9ca5cd6d9c2fed5a2c627d7ef97622b0
khyoo <khyoo1221@gmail.com>
2015-05-06 23:47:44 +0900

    add mpoint.cpp

---
+++ src/mobject/mpoint.cpp
@@ -0,0 +1,28 @@
+
+#include <geos/geom/Point.h>
+#include <geos/geom/Coordinate.h>
+#include <geos/geom/CoordinateArraySequence.h>
+#include <geos/geom/Dimension.h>
+#include <geos/geom/Geometry.h>
+#include <geos/geom/GeometryFactory.h>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+public class MPoint : MObject
+{
+	protected vector<Point> moVector;
+
+	Mpoint();
+	Mpoint(MovingFunctionType functionType)
+};
+
+Mpoint::Mpoint() {
+
+}
+
+Mpoint::Mpoint(MovingFunctionType functionType) {
+	
+}
