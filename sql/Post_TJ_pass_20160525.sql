ae553e1b6148bd305b416697c4ca4b322e1451ba
khyoo <khyoo1221@gmail.com>
2016-05-25 14:59:06 +0900

    commit : 05-25

--- sql/Post_TJ_pass_20160525.sql
+++ sql/Post_TJ_pass_20160525.sql
@@ -9,6 +9,9 @@ DECLARE
 	input_trajectory		alias for $1;
 	input_polygon			alias for $2;
 
+	session_key 		text;
+	session_value		text;
+	
 	traj_prefix			char(10);
 	traj_suffix			char(10);
 	traj_segtable_name	char(50);
@@ -16,19 +19,32 @@ DECLARE
 			
 	cnt				integer;	
 BEGIN		
+
 	traj_prefix := 'mpseq_';
 	traj_suffix := '_traj';
+		
 	traj_segtable_name := traj_prefix || input_trajectory.segtableoid || traj_suffix;
+		
+	session_key := traj_segtable_name;
 	
-	sql_text := ' select count(*) from ' || traj_segtable_name || ' mp where mp.mpid = ' || input_trajectory.moid || ' AND TJ_passes((mp).tpseg, $1)';
+	perform set_config(session_key, session_value, false);
 
-	EXECUTE sql_text INTO cnt USING input_polygon;
+	session_value := current_setting(session_key);
+	
+	if (session_value = null) then
+		perform set_config(session_key, 1, false);
+	else
+		sql_text := ' select count(*) from ' || traj_segtable_name || ' mp where mp.mpid = ' || input_trajectory.moid || ' AND TJ_passes((mp).tpseg, $1)';
+	
+		EXECUTE sql_text INTO cnt USING input_polygon;
 
-	IF cnt > 0 THEN
-		RETURN true;
-	ELSE
-		RETURN false;
-	END IF;
+		IF cnt > 0 THEN
+			RETURN true;
+		ELSE
+			RETURN false;
+		END IF;
+	end if;
+
 END
 $$
 LANGUAGE 'plpgsql';
