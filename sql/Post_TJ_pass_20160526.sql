51e2c7d28b0bfa8372d75dec4b4f5a55d74eb181
khyoo <khyoo1221@gmail.com>
2016-06-01 22:24:47 +0900

    commit : 06-01

--- sql/Post_TJ_pass_20160526.sql
+++ sql/Post_TJ_pass_20160526.sql
@@ -1,12 +1,13 @@
-CREATE OR REPLACE FUNCTION Post_TJ_pass(trajectory, geometry) RETURNS boolean AS
+﻿CREATE OR REPLACE FUNCTION Post_TJ_pass(trajectory, geometry) RETURNS boolean AS
 $$
 DECLARE
 	input_trajectory		alias for $1;
 	input_polygon			alias for $2;
 
+	session_id		text;
 	session_key 		text;
 	session_value		text;
-	
+		
 	traj_prefix			char(10);
 	traj_suffix			char(10);
 	
@@ -17,45 +18,47 @@ DECLARE
 		
 	cnt				integer;	
 BEGIN		
-
-	traj_prefix := 'mpseq_';
-	traj_suffix := '_traj';
+	traj_prefix := current_setting('traj.prefix');
+	traj_suffix := current_setting('traj.suffix');
 		
 	traj_segtable_name := traj_prefix || input_trajectory.segtableoid || traj_suffix;
-		
-	sql_text := 'select box2d($1)';
+	
+	text_from_geom := box2d(input_polygon);
 
-	EXECUTE sql_text INTO text_from_geom USING input_polygon;
+	BEGIN
+		session_id := current_setting('session.id');
+	EXCEPTION when undefined_object then
+		perform set_config('session.id', to_char(CURRENT_TIMESTAMP, 'YYYY-MM-DD HH12:MI:SS'), false);	     
+		session_id := current_setting('session.id');
+	END;
 
-	session_key := traj_segtable_name || '.pass.' || text_from_geom;
+	-- raise notice 'session_id: %', session_id;		
 
+	session_key := session_id || '.' || traj_segtable_name || '.pass.' || text_from_geom;
+
+	-- raise notice 'session_key 2 : %', session_key;		
+	
 	BEGIN
 		session_value := current_setting(session_key);
 	EXCEPTION when undefined_object then
 		perform set_config(session_key, '0', false);	     
 		session_value := current_setting(session_key);
 	END;
-		
+	
+	
 	IF session_value = '0' THEN
 		perform set_config(session_key, '1', false);
 
-		IF EXISTS (SELECT relname FROM pg_class WHERE relname='tmp_segtable') THEN
-			sql_text := 'SELECT mp.mpid FROM ' || traj_segtable_name || ' mp';
-			sql_text := sql_text || ' WHERE mp.rect && $1';
-			--raise notice '%', sql_text;
-		ELSE		
-			sql_text := 'CREATE TEMPORARY TABLE tmp_segtable as ';
-			sql_text := sql_text || 'SELECT mp.mpid FROM ' || traj_segtable_name || ' mp';
-			sql_text := sql_text || ' WHERE mp.rect && $1';
-			--raise notice '%', sql_text;
-		END IF;	
+		sql_text := 'CREATE TEMPORARY TABLE tmp_segtable as ';
+		sql_text := sql_text || 'SELECT mp.mpid FROM ' || traj_segtable_name || ' mp';
+		sql_text := sql_text || ' WHERE mp.rect && $1';
+		-- raise notice '%', sql_text;		
+
+		EXECUTE sql_text USING input_polygon;
 	END IF;
-			
-	EXECUTE sql_text USING input_polygon;
 
 	sql_text := 'SELECT COUNT(*) FROM tmp_segtable tmp WHERE tmp.mpid = ' || input_trajectory.moid;
-
-	raise notice '%', sql_text;
+	
 	EXECUTE sql_text INTO cnt;
 
 	IF cnt > 0 THEN
