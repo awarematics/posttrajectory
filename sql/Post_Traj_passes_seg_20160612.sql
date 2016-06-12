58b7c4f8b2fa55e8292f63f3beccbdb17b74c3f1
khyoo1221 <khyoo1221@gmail.com>
2016-06-12 05:23:14 -0700

    commit : 06-12

---
+++ sql/Post_Traj_passes_seg_20160612.sql
@@ -0,0 +1,63 @@
+﻿
+CREATE OR REPLACE FUNCTION Post_Traj_passes_3d_seg(trajectory, geometry) RETURNS boolean AS
+$$
+DECLARE
+	input_trajectory		alias for $1;
+	input_polygon			alias for $2;
+
+	session_id			text;
+	session_key 			text;
+	session_value			text;
+		
+	traj_prefix			char(10);
+	traj_suffix			char(10);
+	
+	traj_segtable_name		text;
+	
+	sql_text			varchar;
+	text_from_geom			text;
+		
+	cnt				integer;	
+BEGIN		
+	traj_prefix := current_setting('traj.prefix');
+	traj_suffix := current_setting('traj.suffix');
+		
+	traj_segtable_name := traj_prefix || input_trajectory.segtableoid || traj_suffix;
+	
+	text_from_geom := box2d(input_polygon);
+
+	session_key := traj_segtable_name || '.passes.' || text_from_geom;
+
+	-- raise notice 'session_key : %', session_key;		
+	
+	BEGIN
+		session_value := current_setting(session_key);
+	EXCEPTION when undefined_object then
+		perform set_config(session_key, '0', false);	     
+		session_value := current_setting(session_key);
+	END;
+	
+	
+	IF session_value = '0' THEN
+		perform set_config(session_key, '1', false);
+
+		sql_text := 'CREATE TEMPORARY TABLE tmp_segtable as ';
+		sql_text := sql_text || 'SELECT mp.mpid, mp.tpseg FROM ' || traj_segtable_name || ' mp';
+		sql_text := sql_text || ' WHERE tpArr_to_geom(mp.tpseg) &&& $1';
+		-- raise notice '%', sql_text;		
+
+		EXECUTE sql_text USING input_polygon;
+	END IF;
+
+	sql_text := 'SELECT COUNT(*) FROM tmp_segtable tmp WHERE tmp.mpid = ' || input_trajectory.moid || ' AND TJ_Passes(tmp.tpseg, $1)';
+	
+	EXECUTE sql_text INTO cnt USING input_polygon;
+
+	IF cnt > 0 THEN
+		RETURN true;
+	ELSE
+		RETURN false;
+	END IF;	
+END
+$$
+LANGUAGE 'plpgsql';
