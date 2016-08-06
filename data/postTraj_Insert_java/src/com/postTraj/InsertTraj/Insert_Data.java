9e6bd0850aa4316af51d0828043d1108afaab652
khyoo1221 <khyoo1221@gmail.com>
2016-08-06 06:58:16 -0700

    commit : 08-06

---
+++ data/postTraj_Insert_java/src/com/postTraj/InsertTraj/Insert_Data.java
@@ -0,0 +1,760 @@
+package com.postTraj.InsertTraj;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.util.Arrays;
+
+import com.postTraj.DBConnector.DBConnector;
+import com.postTraj.Query.Make_Query;
+import com.postTraj.Query.Tokenize_data;
+
+public class Insert_Data {
+	File[] file_name;
+	String table_name;
+	ResultSet rs, rs1;
+
+	BufferedReader in;
+
+	DBConnector dbconn = new DBConnector();
+
+	public void dbcon() {
+		if (dbconn.dbconnector()) {
+			System.out.println("DB Connect Success!");
+		} else {
+			System.out.println("DB Connect Fail!");
+		}
+	}
+
+	public void dbclose() {
+		System.out.println("DB closed");
+		dbconn.closeDB();
+	}
+
+	public File[] get_fileArr(String path) {
+
+		File f = new File(path);
+
+		File[] result = new File[1];
+
+		File[] pathArr = f.listFiles();
+
+		File tmp_file;
+		String dir_name = "";
+
+		for (int i = 0; i < pathArr.length; i++) {
+
+			if (pathArr[i].isDirectory()) {
+				if (pathArr[i].getAbsolutePath().length() != 5) {
+
+					dir_name = pathArr[i].getName();
+
+					for (; dir_name.length() < 5;) {
+						dir_name = "0" + dir_name;
+					}
+					tmp_file = new File(pathArr[i].getAbsolutePath());
+					tmp_file.renameTo(new File(pathArr[i].getParent() + "/" + dir_name));
+
+				} else {
+					tmp_file = new File(pathArr[i].getAbsolutePath());
+				}
+			}
+		}
+
+		Arrays.sort(pathArr);
+
+		int total = 0;
+
+		for (int i = 0; i < pathArr.length; i++) {
+			if (pathArr[i].isDirectory()) {
+				// System.out.println(pathArr[i].getAbsolutePath());
+				total += pathArr[i].list().length;
+			} else {
+				total++;
+			}
+		}
+		// System.out.println(total);
+
+		result = new File[total];
+
+		File[] tmpArr;
+		int srcPos = 0;
+
+		for (int i = 0; i < pathArr.length; i++) {
+			tmpArr = pathArr[i].listFiles();
+
+			Arrays.sort(tmpArr);
+			for (int j = 0; j < tmpArr.length; j++) {
+				result[srcPos] = tmpArr[j];
+				srcPos++;
+			}
+		}
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(i + " : " + result[i]);
+		}
+
+		return result;
+	}
+
+	/*
+	 * Insert Data of files
+	 * 
+	 */
+	public void data_insert(int cnt) {
+
+		long start = System.currentTimeMillis();
+
+		int segId = 0, next_segId = 0, before_segId = 0, mpId = 0;
+
+		String start_time = "", end_time = "";
+
+		Make_Query query = new Make_Query();
+
+		String filePath = "/root/DataSet/";
+		file_name = get_fileArr(filePath);
+
+		for (int t = 0; t < file_name.length; t++) {
+			// System.out.println(file_name[t].getAbsolutePath());
+			try {
+				in = new BufferedReader(new FileReader(file_name[t]));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			int print_cnt = 0;
+
+			segId = 0;
+
+			String data;
+
+			boolean isTaxiNum = false;
+
+			try {
+
+				int tp_cnt = 0;
+
+				String ptArr = "", tpArr = "", start_pt = "";
+
+				Tokenize_data tokenized = new Tokenize_data();
+
+				while ((data = in.readLine()) != null) {
+
+					if (!isTaxiNum) {
+						isTaxiNum = true;
+
+						try {
+							rs = dbconn.queryExecute(query.find_TaxiNum(data, "taxi"));
+
+							while (rs.next()) {
+
+								if (rs.getInt(1) == 0) {
+									rs1 = dbconn.queryExecute(query.find_MaxId("taxi_id", "taxi"));
+
+									while (rs1.next()) {
+										int taxiId = rs1.getInt(1) + 1;
+										// System.out.println("Insert record in
+										// taxi table");
+
+										dbconn.queryUpdate(query.insert_Taxi(data, "taxi", taxiId));
+									}
+
+									rs1 = dbconn.queryExecute(query.get_seqNum("taxi"));
+
+									while (rs1.next()) {
+
+										mpId = rs1.getInt(1);
+									}
+								} else {
+
+								}
+							}
+						} catch (Exception e) {
+							System.out.println("Exception");
+						}
+					}
+
+					if (tp_cnt < cnt) {
+						tp_cnt++;
+
+						tokenized.tokenize(data);
+
+						if (tp_cnt == 1) {
+							start_pt = tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+							start_time = tokenized.getDate_str();
+						}
+
+						tpArr += "( tpoint(st_point(" + tokenized.getLatitude() + ", " + tokenized.getLongitude()
+								+ "), timestamp '" + tokenized.getDate_str() + "') )";
+
+						ptArr += tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+						if (tp_cnt < cnt) {
+
+							tpArr += ",";
+
+							ptArr += ",";
+						}
+
+						end_time = tokenized.getDate_str();
+
+						print_cnt++;
+						// System.out.println(print_cnt);
+					} else if (tp_cnt >= cnt) {
+
+						segId++;
+
+						next_segId = segId + 1;
+
+						before_segId = segId - 1;
+
+						ptArr += ", " + start_pt;
+
+						dbconn.queryUpdate(query.insert_traj(mpId, segId, next_segId, before_segId, tp_cnt,
+								query.make_polygon(ptArr), start_time, end_time, query.make_tpseg(tpArr)));
+
+						tp_cnt = 1;
+
+						tokenized.tokenize(data);
+
+						start_pt = tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+						start_time = tokenized.getDate_str();
+
+						tpArr = "( tpoint(st_point(" + tokenized.getLatitude() + ", " + tokenized.getLongitude()
+								+ "), timestamp '" + tokenized.getDate_str() + "') )";
+
+						ptArr = tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+						if (tp_cnt < cnt) {
+
+							tpArr += ",";
+							ptArr += ",";
+						}
+						// System.out.println(print_cnt);
+						print_cnt++;
+
+					}
+				}
+
+				if (tp_cnt < cnt) {
+					segId++;
+
+					next_segId = segId + 1;
+
+					before_segId = segId - 1;
+
+					ptArr += start_pt;
+
+					if (tpArr.length() > 0) {
+						tpArr = tpArr.substring(0, tpArr.lastIndexOf(','));
+
+						end_time = tokenized.getDate_str();
+
+						dbconn.queryUpdate(query.insert_traj(mpId, segId, next_segId, before_segId, tp_cnt,
+								query.make_polygon(ptArr), start_time, end_time, query.make_tpseg(tpArr)));
+					}
+					// System.out.println(query.insert_traj(mpId, segId,
+					// next_segId,
+					// before_segId, tp_cnt, query.make_polygon(ptArr),
+					// tokenized.getDate_str(), tokenized.getDate_str(),
+					// query.make_tpseg(tpArr)));
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		long end = System.currentTimeMillis();
+
+		System.out.println("Total Time : " + (end - start) / 1000.0);
+	}
+
+	/*
+	 * Insert Data of a file
+	 * 
+	 */
+	public void data_insert(int cnt, String filePath) {
+
+		int segId = 0, next_segId = 0, before_segId = 0, mpId = 0;
+
+		String start_time = "", end_time = "";
+
+		Make_Query query = new Make_Query();
+
+		try {
+			in = new BufferedReader(new FileReader(filePath));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		int print_cnt = 0;
+
+		String data;
+
+		boolean isTaxiNum = false;
+
+		long start = System.currentTimeMillis();
+
+		try {
+
+			int tp_cnt = 0;
+
+			String ptArr = "", tpArr = "", start_pt = "";
+
+			Tokenize_data tokenized = new Tokenize_data();
+
+			while ((data = in.readLine()) != null) {
+
+				if (!isTaxiNum) {
+					isTaxiNum = true;
+
+					try {
+						rs = dbconn.queryExecute(query.find_TaxiNum(data, "taxi"));
+
+						while (rs.next()) {
+
+							if (rs.getInt(1) == 0) {
+								rs1 = dbconn.queryExecute(query.find_MaxId("taxi_id", "taxi"));
+
+								while (rs1.next()) {
+									int taxiId = rs1.getInt(1) + 1;
+									System.out.println("Insert record in taxi table");
+
+									dbconn.queryUpdate(query.insert_Taxi(data, "taxi", taxiId));
+								}
+
+								rs1 = dbconn.queryExecute(query.get_seqNum("taxi"));
+
+								while (rs1.next()) {
+
+									mpId = rs1.getInt(1);
+								}
+							} else {
+
+							}
+						}
+					} catch (Exception e) {
+						System.out.println("Exception");
+					}
+				}
+
+				if (tp_cnt < cnt) {
+					tp_cnt++;
+
+					tokenized.tokenize(data);
+
+					if (tp_cnt == 1) {
+						start_pt = tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+						start_time = tokenized.getDate_str();
+					}
+
+					tpArr += "( tpoint(st_point(" + tokenized.getLatitude() + ", " + tokenized.getLongitude()
+							+ "), timestamp '" + tokenized.getDate_str() + "') )";
+
+					ptArr += tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+					if (tp_cnt < cnt) {
+
+						tpArr += ",";
+
+						ptArr += ",";
+					}
+
+					end_time = tokenized.getDate_str();
+
+					print_cnt++;
+					System.out.println(print_cnt);
+				} else if (tp_cnt >= cnt) {
+
+					segId++;
+
+					next_segId = segId + 1;
+
+					before_segId = segId - 1;
+
+					ptArr += ", " + start_pt;
+
+					dbconn.queryUpdate(query.insert_traj(mpId, segId, next_segId, before_segId, tp_cnt,
+							query.make_polygon(ptArr), start_time, end_time, query.make_tpseg(tpArr)));
+
+					tp_cnt = 1;
+
+					tokenized.tokenize(data);
+
+					start_pt = tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+					start_time = tokenized.getDate_str();
+
+					tpArr = "( tpoint(st_point(" + tokenized.getLatitude() + ", " + tokenized.getLongitude()
+							+ "), timestamp '" + tokenized.getDate_str() + "') )";
+
+					ptArr = tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+					if (tp_cnt < cnt) {
+
+						tpArr += ",";
+						ptArr += ",";
+					}
+
+					System.out.println(print_cnt);
+					print_cnt++;
+
+				}
+			}
+
+			if (tp_cnt < cnt) {
+				segId++;
+
+				next_segId = segId + 1;
+
+				before_segId = segId - 1;
+
+				ptArr += start_pt;
+
+				tpArr = tpArr.substring(0, tpArr.lastIndexOf(','));
+
+				end_time = tokenized.getDate_str();
+
+				dbconn.queryUpdate(query.insert_traj(mpId, segId, next_segId, before_segId, tp_cnt,
+						query.make_polygon(ptArr), start_time, end_time, query.make_tpseg(tpArr)));
+
+			}
+			System.out.println(query.insert_traj(mpId, segId, next_segId, before_segId, tp_cnt,
+					query.make_polygon(ptArr), start_time, end_time, query.make_tpseg(tpArr)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+
+		System.out.println("Total Time : " + (end - start) / 1000.0);
+	}
+	
+	/*
+	 * exist table
+	 * 
+	 */
+	public boolean exist_table(String schemeName, String tbName) {
+		
+		Make_Query query = new Make_Query();
+		
+		boolean bool = true;
+		
+		try {							
+			rs = dbconn.queryExecute(query.is_table(schemeName, tbName));
+			
+			while (rs.next()) {
+				if (rs.getInt(1) == 0) {
+					dbconn.queryUpdate(query.create_trajDataTable(tbName));
+					
+					bool = true;
+				}
+			}
+		} catch (Exception e) {
+			bool = false;
+		}	
+		
+		return bool;
+	}
+	
+
+	/*
+	 * insert_mqseqTrajData of a file
+	 * 
+	 */
+	public void insert_mpseqTrajData(int cnt, String filePath) {
+
+		Make_Query query = new Make_Query();
+
+		try {
+			in = new BufferedReader(new FileReader(filePath));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int print_cnt = 0;
+
+		String data;
+
+		boolean isTaxiNum = false;
+
+		try {
+			int tp_cnt = 0;
+
+			String tpArr = "", ptArr = "", startPt = "";
+
+			Tokenize_data tokenized = new Tokenize_data();
+
+			boolean isStart = true;
+
+			double beforeLati = 0.0, beforeLong = 0.0;
+
+			while ((data = in.readLine()) != null) {
+
+				/*
+				 * if (!isTaxiNum) { isTaxiNum = true;
+				 * 
+				 * try { rs = dbconn.queryExecute(query.find_TaxiNum(data,
+				 * "taxi"));
+				 * 
+				 * while (rs.next()) {
+				 * 
+				 * if (rs.getInt(1) == 0) { rs1 =
+				 * dbconn.queryExecute(query.find_MaxId("taxi_id", "taxi"));
+				 * 
+				 * while (rs1.next()) { int taxiId = rs1.getInt(1) + 1;
+				 * System.out.println("Insert record in taxi table");
+				 * 
+				 * dbconn.queryUpdate(query.insert_Taxi(data, "taxi", taxiId));
+				 * }
+				 * 
+				 * rs1 = dbconn.queryExecute(query.get_seqNum("taxi"));
+				 * 
+				 * while (rs1.next()) {
+				 * 
+				 * mpId = rs1.getInt(1); } } else {
+				 * 
+				 * } } } catch (Exception e) { System.out.println("Exception");
+				 * } }
+				 */
+
+				tokenized.tokenize(data);
+
+				if (Double.parseDouble(tokenized.getLatitude()) <= 1
+						|| Double.parseDouble(tokenized.getLongitude()) <= 1) {
+					continue;
+				}
+
+				if (tp_cnt < cnt) {
+
+					if (tp_cnt > 1) {
+						if (Math.abs(beforeLati - Double.parseDouble(tokenized.getLatitude())) > 1
+								|| Math.abs(beforeLong - Double.parseDouble(tokenized.getLongitude())) > 1) {
+							continue;
+						}
+					}
+
+					if (!isStart) {
+						tpArr += ",";
+						ptArr += ",";
+					}
+
+					isStart = false;
+
+					tp_cnt++;
+
+					tpArr += "( tpoint(st_point(" + tokenized.getLatitude() + ", " + tokenized.getLongitude()
+							+ "), timestamp '" + tokenized.getDate_str() + "') )";
+					ptArr += tokenized.getLatitude() + " " + tokenized.getLongitude();
+
+					beforeLati = Double.parseDouble(tokenized.getLatitude());
+					beforeLong = Double.parseDouble(tokenized.getLongitude());
+
+					// print_cnt++;
+					// System.out.println(print_cnt);
+
+				} else if (tp_cnt == cnt) {
+
+					// System.out.println(ptArr.split(",").length);
+					startPt = ptArr.split(",")[0];
+
+					ptArr += "," + startPt;
+					
+//					System.out.println(query.makeQuery_insertMpseqTrajData(tokenized.getOid(), tp_cnt,
+//							query.make_polygon(ptArr), query.make_tpseg(tpArr)));
+					
+					dbconn.queryUpdate(query.makeQuery_insertMpseqTrajData(tokenized.getOid(), tp_cnt,
+							query.make_polygon(ptArr), query.make_tpseg(tpArr)));
+
+					if (Math.abs(beforeLati - Double.parseDouble(tokenized.getLatitude())) > 1
+							|| Math.abs(beforeLong - Double.parseDouble(tokenized.getLongitude())) > 1) {
+						continue;
+					}
+
+					beforeLati = Double.parseDouble(tokenized.getLatitude());
+					beforeLong = Double.parseDouble(tokenized.getLongitude());
+
+					tp_cnt = 1;
+
+					tpArr = "( tpoint(st_point(" + tokenized.getLatitude() + ", " + tokenized.getLongitude()
+							+ "), timestamp '" + tokenized.getDate_str() + "') )";
+					ptArr = tokenized.getLatitude() + " " + tokenized.getLongitude();
+				}
+			}
+
+			// System.out.println(ptArr.split(",").length);
+
+			startPt = ptArr.split(",")[0];
+
+			ptArr += "," + startPt;
+
+//			System.out.println(query.makeQuery_insertMpseqTrajData(tokenized.getOid(), tp_cnt,
+//					query.make_polygon(ptArr), query.make_tpseg(tpArr)));
+			
+			dbconn.queryUpdate(query.makeQuery_insertMpseqTrajData(tokenized.getOid(), tp_cnt,
+					query.make_polygon(ptArr), query.make_tpseg(tpArr)));
+
+			// System.out
+			// .println(query.makeQuery_insertMpseqTrajData(tokenized.getOid(),
+			// tp_cnt, query.make_tpseg(tpArr)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/*
+	 * 
+	 */
+	// public void insert_files(int cnt, String path) {
+	//
+	// File f = new File(path);
+	//
+	// File[] result = new File[1];
+	//
+	// File[] pathArr = f.listFiles();
+	//
+	// File tmp_file;
+	// String dir_name = "";
+	//
+	// for (int i = 0; i < pathArr.length; i++) {
+	//
+	// if (pathArr[i].isDirectory()) {
+	// if (pathArr[i].getAbsolutePath().length() != 5) {
+	//
+	// dir_name = pathArr[i].getName();
+	//
+	// for (; dir_name.length() < 5;) {
+	// dir_name = "0" + dir_name;
+	// }
+	// tmp_file = new File(pathArr[i].getAbsolutePath());
+	// tmp_file.renameTo(new File(pathArr[i].getParent() + "/" + dir_name));
+	//
+	// } else {
+	// tmp_file = new File(pathArr[i].getAbsolutePath());
+	// }
+	// }
+	// }
+	//
+	// Arrays.sort(pathArr);
+	//
+	// int total = 0;
+	//
+	// for (int i = 0; i < pathArr.length; i++) {
+	// if (pathArr[i].isDirectory()) {
+	// // System.out.println(pathArr[i].getAbsolutePath());
+	// total += pathArr[i].list().length;
+	// } else {
+	// total++;
+	// }
+	// }
+	// // System.out.println(total);
+	//
+	// result = new File[total];
+	//
+	// File[] tmpArr;
+	// int srcPos = 0;
+	//
+	// for (int i = 0; i < pathArr.length; i++) {
+	// tmpArr = pathArr[i].listFiles();
+	//
+	// Arrays.sort(tmpArr);
+	// for (int j = 0; j < tmpArr.length; j++) {
+	// result[srcPos] = tmpArr[j];
+	// srcPos++;
+	// }
+	// }
+	//
+	// for (int i = 0; i < result.length; i++) {
+	// insert_mqseqTrajData(1000, result[i].toString());
+	// // System.out.println(i + " : " + result[i]);
+	// }
+	//
+	// // return result;
+	// }
+
+	public void insert_files(int cnt, String path) {
+
+		File f = new File(path);
+
+		File[] result = new File[1];
+
+		File[] pathArr = f.listFiles();
+
+		File tmp_file;
+		String dir_name = "";
+
+		for (int i = 0; i < pathArr.length; i++) {
+
+			if (pathArr[i].isDirectory()) {
+				if (pathArr[i].getAbsolutePath().length() != 5) {
+
+					dir_name = pathArr[i].getName();
+
+					for (; dir_name.length() < 5;) {
+						dir_name = "0" + dir_name;
+					}
+					tmp_file = new File(pathArr[i].getAbsolutePath());
+					tmp_file.renameTo(new File(pathArr[i].getParent() + "/" + dir_name));
+
+				} else {
+					tmp_file = new File(pathArr[i].getAbsolutePath());
+				}
+			}
+		}
+
+		Arrays.sort(pathArr);
+
+		int total = 0;
+
+		for (int i = 0; i < pathArr.length; i++) {
+			if (pathArr[i].isDirectory()) {
+				// System.out.println(pathArr[i].getAbsolutePath());
+				total += pathArr[i].list().length;
+			} else {
+				// System.out.println(total + "file: " +
+				// pathArr[i].getAbsolutePath());
+				total++;
+			}
+		}
+		System.out.println(total);
+
+		result = new File[total];
+
+		File[] tmpArr;
+		int srcPos = 0;
+
+		for (int i = 0; i < pathArr.length; i++) {
+			if (pathArr[i].isDirectory()) {
+				tmpArr = pathArr[i].listFiles();
+
+				Arrays.sort(tmpArr);
+				for (int j = 0; j < tmpArr.length; j++) {
+					result[srcPos] = tmpArr[j];
+					srcPos++;
+				}
+			} else {
+				result[srcPos] = pathArr[i];
+				srcPos++;
+
+			}
+		}
+
+		for (int i = 0; i < result.length; i++) {
+			insert_mpseqTrajData(cnt, result[i].toString());
+			// System.out.println(i + " : " + result[i]);
+		}
+	
+		// return result;
+	}
+
+}
---
+++ data/postTraj_Insert_java/src/com/postTraj/Query/Make_Query.java
@@ -0,0 +1,98 @@
+package com.postTraj.Query;
+
+public class Make_Query {
+	String query;
+	String modify_Query_Array_Data;
+
+	Tokenize_data tokenized = new Tokenize_data();
+
+	public String get_seqNum(String table_name) {
+		
+		query = "select currval('taxi_traj_mpointid_seq');";
+
+		return query;
+	}
+	
+	public String find_TaxiNum(String data, String table_name) {
+		tokenized.tokenize(data);
+
+		query = "select count(*) from " + table_name + " where taxi_number = '" + tokenized.getOid() + "';";
+
+		return query;
+	}
+
+	public String find_MaxId(String maxId, String table_name) {
+
+		query = "select Max(" + maxId + ") from " + table_name;
+
+		return query;
+	}
+
+	public String insert_Taxi(String data, String table_name, int idx) {
+		tokenized.tokenize(data);
+
+		query = "insert into " + table_name + " values (" + idx + ", '" + tokenized.getOid() + "', '"
+				+ tokenized.getOid() + "', '" + tokenized.getOid() + "');";
+
+		return query;
+	}
+
+	public String insert_traj(int mpId, int segId, int next_segId, int before_segId, int mpCnt, String rect,
+			String start_time, String end_time, String tpseg) {
+
+		query = "insert into mpseq_527042_traj values (" + mpId + ", " + segId + ", " + next_segId + ", " + before_segId
+				+ ", " + mpCnt + ", " + rect + ", " + rect + ", timestamp '" + start_time + "', timestamp '" + end_time + "', "
+				+ tpseg + ");";
+
+		return query;
+	}
+
+	public String is_table(String schemeName, String tbName) {
+		query = "SELECT COUNT(*) FROM pg_tables WHERE schemaname = '" + schemeName + "' AND tablename = '" + tbName + "';";
+
+		return query;
+	}
+	
+	public String create_trajDataTable(String tbName) {
+		query = "CREATE TABLE public." + tbName + " ( ";
+		query += "id bigserial, ";
+		query += "carnumber character(100), ";
+		query += "mpcount integer, ";
+		query += "rect geometry, ";
+		query += "tpseg tpoint[] ";
+		query += ");";
+		
+		return query;
+	}
+	
+	public String make_polygon(String q) {
+		query = "box2d(ST_GeomFromText('LINESTRING(" + q + ")'))";
+
+		return query;
+	}
+
+	public String make_tpseg(String q) {
+
+		query = "ARRAY[ " + q + " ]::tpoint[]";
+
+		return query;
+	}
+
+	public String make_appendquery(String data, String table_name, String traj_column_name) {
+		tokenized.tokenize(data);
+
+		query = "update " + table_name + " set " + traj_column_name + " = " + "append(" + traj_column_name
+				+ ", tpoint(st_point(" + tokenized.getLatitude() + ", " + tokenized.getLongitude() + "), "
+				+ "timestamp '" + tokenized.getDate_str() + "' ) ) where taxi_number = '" + tokenized.getOid() + "';";
+
+		return query;
+	}
+	
+	public String makeQuery_insertMpseqTrajData(String carNumber, int mpCount, String rect, String tpsegArr) {
+
+		query = "insert into mpseq_traj_data(carnumber, mpcount, rect, tpseg) values (" + carNumber + ", " + mpCount + ", " + rect + ", " + tpsegArr + ");";
+
+		return query;
+	}
+
+}
