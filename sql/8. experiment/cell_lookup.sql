e473c0d82248e8ed9dcbb7a2e42e12c4d1a84105
khyoo1221 <khyoo1221@gmail.com>
2016-10-17 22:02:41 +0900

    commit : 10-17

---
+++ sql/8. experiment/cell_lookup.sql
@@ -0,0 +1,38 @@
+﻿-- select cell_lookup(geometry(ST_MakeBox2D(ST_Point(108.45223, 22.91412), ST_Point(117.589875, 34.351745))));
+-- select count(*) from cell_lookup(geometry(ST_MakeBox2D(ST_Point(108.45223, 22.91412), ST_Point(117.589875, 34.351745))));
+
+
+DROP FUNCTION cell_lookup(box2d);
+
+CREATE OR REPLACE FUNCTION public.cell_lookup(box2d)
+  RETURNS setof integer AS
+$$
+DECLARE
+	rect		alias for $1;
+
+	tmp_conf	varchar;
+	tmp_box		box2d;
+	
+	cell_id		integer;
+	
+	sql		varchar;
+BEGIN
+	FOR i IN 1..100 LOOP
+		sql := 'ptraj.cell_' || i;
+		
+		tmp_conf := current_setting(sql);
+
+		tmp_box := ST_MakeBox2D(ST_PointFromText(split_part(tmp_conf, ',',1)), ST_PointFromText(split_part(tmp_conf, ',',2)));
+		-- raise notice '%', tmp_box;
+
+		IF (ST_intersects(rect, tmp_box)) THEN
+			cell_id := i;
+			-- raise notice '%', cell_id;
+			return next cell_id;
+		END IF;
+	END LOOP;
+END
+$$
+LANGUAGE plpgsql IMMUTABLE
+  COST 100;
+
---
+++ sql/8. experiment/getQuery_Condition_cells.sql
@@ -0,0 +1,35 @@
+﻿
+-- select * from getQuery_Condition_cells(geometry(ST_MakeBox2D(ST_Point(108.45223, 22.91412), ST_Point(110.7366413, 25.77352625))));
+
+-- Function: public.getquery_condition_cells(box2d)
+
+-- DROP FUNCTION public.getquery_condition_cells(box2d);
+
+CREATE OR REPLACE FUNCTION public.getquery_condition_cells(box2d)
+  RETURNS SETOF mpseq_1101121_traj AS
+$BODY$
+DECLARE
+	rect				alias for $1;
+
+	cond_cell			text;
+	sql				text;
+	
+	r				mpseq_1101121_traj%rowtype;
+BEGIN
+	cond_cell := getCondition_cells_v2(geometry(rect));
+
+	-- raise notice '%', cond_cell;
+
+	sql := 'select * from mpseq_1101121_traj where (' || cond_cell || ') and TJ_Passes(tpseg, geometry($1));';
+	-- raise notice '%', sql;
+	
+	FOR r IN execute sql using rect LOOP				
+		return next r;
+	END LOOP;		
+END
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION public.getquery_condition_cells(box2d)
+  OWNER TO postgres;
