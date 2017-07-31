cd98bc3fc22348eba3baad549d8d14a76bd200f7
manner7979 <manner7979@gmail.com>
2017-07-31 22:39:49 +0900

    create table change

--- sql/0. install/2. Install_ptraj_tables.sql
+++ sql/0. install/2. Install_ptraj_tables.sql
@@ -1,39 +1,3 @@
-﻿-- Table: public.mpseq_traj_data
-
--- DROP TABLE public.mpseq_traj_data;
-
-CREATE TABLE public.mpseq_traj_data
-(
-  id bigserial,
-  carnumber char(100),  
-  mpcount integer,
-  rect geometry,
-  tpseg tpoint[]
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE public.mpseq_traj_data
-  OWNER TO postgres;
-
-
--- Table: public.static_partition
-
--- DROP TABLE public.static_partition;
-
-CREATE TABLE public.static_partition
-(
-  id integer,
-  partition box2d
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE public.static_partition
-  OWNER TO postgres;
-
-
-
 
 -- TABLE trajectory_columns 정의 --
 CREATE TABLE trajectory_columns
@@ -54,16 +18,3 @@ CREATE TABLE trajectory_columns
 WITH (
   OIDS=TRUE
 );
-
-
---- 테스트용 데이터
-
-create table taxi(
-	taxi_id integer,
-	taxi_number varchar,
-	taxi_model varchar,
-	taxi_driver varchar
-);
-
--- 컬럼 추가
-select addtrajectorycolumn('public', 'taxi', 'traj', 4326, 'MOVINGPOINT', 2, 150);
