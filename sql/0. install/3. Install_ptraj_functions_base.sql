c24a8b63d65b1ce88ccbf1fe1d8f7e97084ecbef
manner7979 <manner7979@gmail.com>
2017-08-11 15:35:12 +0900

    Update 3. Install_ptraj_functions_base.sql

--- sql/0. install/3. Install_ptraj_functions_base.sql
+++ sql/0. install/3. Install_ptraj_functions_base.sql
@@ -36,7 +36,6 @@ CREATE OR REPLACE FUNCTION tpoint_to_linestring(tpoint[]) RETURNS geometry;
 */
 
 
--- tpoint[]를 받아서 box2d(geometry)를 리턴해준다.
 CREATE OR REPLACE FUNCTION getBox2D(tpoint[]) RETURNS geometry AS
 $$
 DECLARE
@@ -71,13 +70,7 @@ END
 $$
 LANGUAGE 'plpgsql';
 
-/*
-	Trigger Procedure : delete_mpoint_seg()
-	Modify by : khyoo1221@gmail.com
-	Modified Date : -
 
-	To Do................
-*/
 
 CREATE OR REPLACE FUNCTION delete_mpoint_seg() RETURNS trigger AS $delete_mpoint_seg$
 DECLARE		
@@ -109,12 +102,6 @@ DECLARE
     END;
 $delete_mpoint_seg$ LANGUAGE plpgsql;
 
-
-/*
-	Trigger Procedure : Insert_Trigger()
-	Modify by : khyoo1221@gmail.com
-	Modified Date : 2016-05-22
-*/
 
 CREATE OR REPLACE FUNCTION public.insert_trigger()
   RETURNS trigger AS
@@ -159,10 +146,9 @@ END
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-
-
-
--- AddTrajectoryColumn 함수 array 크기를 지정해주지 않는다.
+  
+  
+  
 CREATE OR REPLACE FUNCTION AddTrajectoryColumn(character varying, character varying, character varying, 
 						integer, character varying, integer)
   RETURNS text AS
@@ -175,7 +161,7 @@ DECLARE
 	new_type 	alias for $5;
 	dimension 	alias for $6;
 	
-	resultvalue	text;
+	resultvalue text;
 BEGIN
 
 	execute 'select addTrajectoryColumn($1, $2, $3, $4, $5, $6, 10)'
@@ -192,7 +178,6 @@ COMMENT ON FUNCTION AddTrajectoryColumn(character varying, character varying, ch
 					'args: schema_name, table_name, column_name, srid, type, dimentrion';
 
 
--- AddTrajectoryColumn 함수 tpseg 크기를 지정해줘야 한다.
 CREATE OR REPLACE FUNCTION AddTrajectoryColumn(character varying, character varying, character varying, 
 						integer, character varying, integer, integer)
   RETURNS text AS
@@ -383,13 +368,7 @@ $BODY$
 ALTER FUNCTION AddTrajectoryColumn(character varying, character varying, character varying, integer, character varying, integer) OWNER TO postgres;
 COMMENT ON FUNCTION AddTrajectoryColumn(character varying, character varying, character varying, integer, character varying, integer) IS 
 					'args: schema_name, table_name, column_name, srid, type, dimentrion';
-
-
-
-/*
-	두개 text를 받아서 tpoint 형으로 리턴해주는 함수
-*/
-
+                    
 
 CREATE OR REPLACE FUNCTION tpoint(geometry, timestamp) RETURNS tpoint AS
 $$
@@ -406,11 +385,8 @@ END
 $$
   LANGUAGE 'plpgsql' VOLATILE STRICT
   COST 100;
+  
 
-
-/*
-	trajcetory와 tpoint를 받아서 입력해주는 함수
-*/
 
 CREATE OR REPLACE FUNCTION append(trajectory, geometry, timestamp) RETURNS trajectory AS
 $$
@@ -426,9 +402,6 @@ $$
 LANGUAGE 'plpgsql' VOLATILE STRICT
 COST 100;
 
-
-
--- 인자값으로 tpoint의 array를 받는 append함수
 CREATE OR REPLACE FUNCTION append(trajectory, tpoint[]) RETURNS trajectory AS
 $$
 DECLARE
@@ -458,13 +431,6 @@ END
 $$
 LANGUAGE 'plpgsql' VOLATILE STRICT
 COST 100;
-
-
-/*
-	FUNCTION : append(trajectory, tpoint)
-	Modify by : khyoo1221@gmail.com
-	Modified Date : 2016-08-12
-*/
 
 
 CREATE OR REPLACE FUNCTION append(trajectory, tpoint) RETURNS trajectory AS
@@ -574,7 +540,6 @@ COST 100;
 ALTER FUNCTION append(trajectory, tpoint) OWNER TO postgres;
 
 
--- 삭제시 시작시간과 시간간격(초단위)을 입력하면 삭제를 해준다.
 CREATE OR REPLACE FUNCTION remove(trajectory, timestamp, integer) RETURNS trajectory AS
 $$
 DECLARE
@@ -729,323 +694,7 @@ $$
 LANGUAGE 'plpgsql' 
 
 
-CREATE OR REPLACE FUNCTION modify(trajectory, tpoint) RETURNS trajectory AS
-$$
-DECLARE
-	c_trajectory			alias for $1;
-	c_tpoint			alias for $2;
-	
-	traj_prefix			char(50);
-	traj_suffix			char(50);
-	
-	c_trajectory_segtable_name	char(200);
-
-	sql				text;
-	i				integer;
-	tpseg_size			integer;
-	tp_seg				tpoint[];
-	c_rect				geometry;
-	data				RECORD;
-	
-BEGIN
-
-	-- traj_prefix := current_setting('traj.prefix');
-	-- traj_suffix := current_setting('traj.suffix');
-	traj_prefix := 'mpseq_' ;
-		
-	c_trajectory_segtable_name := traj_prefix || c_trajectory.segtableoid || traj_suffix;
-	
-	sql := 'select * from ' || quote_ident(c_trajectory_segtable_name) || ' where mpid = ' || c_trajectory.moid;
-	
-	FOR data IN EXECUTE sql LOOP
-		i := 1;
-		execute 'select array_upper( $1 , 1)'
-		into tpseg_size using data.tpseg;
-		tp_seg := data.tpseg;
-		-- 현 ROW의 시작시간과 끝시간을 가져와서 변경될 포인트의 시간이 포함되는지 확인한다.
-		-- 조건을 만족하면 현 tpseg의 포인트가 변경되어야 함을 나타낸다. 
-		IF( c_tpoint.ptime >= data.start_time AND c_tpoint.ptime <= data.end_time ) THEN
-			-- tpseg에 변화가 생기면 rect를 변경해주기 위하여 현재의 rect를 불러온다.
-
-
-			WHILE( i <= tpseg_size  ) LOOP
-				IF( tp_seg[i].ts = c_tpoint.ts ) THEN
-					execute 'update ' || quote_ident(c_trajectory_segtable_name) || 
-						' set tpseg[$1] = $2 where mpid = $3 and segid = $4'
-					using i, c_tpoint, data.mpid, data.segid;
-
-				END IF;
-				-- 기존의 box2d에서 새로운 점으로 tpseg가 변경되면 그 점을 포함한 box2d로 변경을 해준다.
-				-- 새로운 점이 들어옴으로써 tpseg를 전부 다시 계산하여 box2d를 만들어준다. 
-				-- 많은 연산이 필요하다는 단점이 있다.
-				IF( i = 1) THEN
-					execute 'update ' || quote_ident(c_trajectory_segtable_name) ||
-						' set rect = ( select st_makebox2d( $1, $1) ) ' ||
-						' where mpid = $2 and segid = $3'
-					using tp_seg[i].pnt, data.mpid, data.segid;
-				ELSE
-					execute 'select rect from ' || quote_ident(c_trajectory_segtable_name) ||
-						' where mpid = $1 and segid = $2' 
-					into c_rect using data.mpid, data.segid;
-					execute 'update ' || quote_ident(c_trajectory_segtable_name) ||
-						' set rect = ( select st_combine_bbox( $1::box2d, $2.p ) ) ' ||
-						' where mpid = $3 and segid = $4'
-					using c_rect, c_tpoint, data.mpid, data.segid;
-
-				END IF;
-				i := i+1;
-			END LOOP;
-		END IF;
-	END LOOP;
-
-	RETURN c_trajectory;
-
-END
-$$
-LANGUAGE 'plpgsql';
-
-
-CREATE OR REPLACE FUNCTION modify(trajectory, tpoint[]) RETURNS trajectory AS
-$$
-DECLARE
-	c_trajectory	alias for $1;
-	array_tpoint	alias for $2;
-
-	array_size	integer;
-	i		integer;
-	sql		text;
-BEGIN
-	execute 'select array_upper( $1, 1)'
-	into array_size using array_tpoint;
-
-	i := 1;
-
-	WHILE( i <= array_size ) LOOP
-		execute 'select modify( $1, $2[$3] )'
-		using c_trajectory, array_tpoint, i;
-		 i := i+1;
-	END LOOP;
-
-	RETURN c_trajectory;
-END
-$$
-LANGUAGE 'plpgsql';
-
-
--- 데이터의 수정 함수, trajectory와 시작시간, 끝시간, 변경할 데이터를 입력해야 한다.
-CREATE OR REPLACE FUNCTION modify(trajectory, timestamp, timestamp, tpoint[]) RETURNS trajectory AS
-$$
-DECLARE
-	c_trajectory			alias for $1;
-	c_start_time			alias for $2;
-	c_end_time			alias for $3;
-	c_tpoint			alias for $4;
-	
-	traj_prefix			char(50);
-	traj_suffix			char(50);
-	
-	c_trajectory_segtable_name	char(200);
-
-	tpoint_max_size			integer;
-	tpoint_min_size			integer;
-	sql				text;
-	i				integer;
-	tpseg_size			integer;
-	tp_seg				tpoint[];
-	data				RECORD;
-	tpseg_max_size			integer;
-	--tpseg를 임시 저장하기 위한 temp
-	tpseg_temp1			tpoint[];
-	tpseg_temp2			tpoint[];
-	tpseg_temp3			tpoint[];
-	tpseg_temp3_size		integer;
-	new_tpseg			tpoint[];
-	-- 새로운 tpseg의 크기를 알기 위한 변수
-	new_tpseg_size			integer;
-	-- 새로운 row 생성시
-	new_rect			geometry;
-	max_segid			integer;
-	new_next_segid			integer;
-	new_before_segid		integer;
-	new_row_segid			integer;
-BEGIN
-	-- traj_prefix := current_setting('traj.prefix');
-	-- traj_suffix := current_setting('traj.suffix');
-		
-	traj_prefix := 'mpseq_' ;
-			
-	c_trajectory_segtable_name := traj_prefix || c_trajectory.segtableoid || traj_suffix;
-	
-	EXECUTE 'select array_upper($1, 1)'
-	INTO tpoint_max_size USING c_tpoint; 
-
-	EXECUTE 'select array_lower($1, 1)'
-	INTO tpoint_min_size USING c_tpoint;
-
-
-	sql := 'select tpseg_size from trajectory_columns ' ||
-		' where f_trajectory_segtable_name = ' || quote_literal(c_trajectory_segtable_name);
-	RAISE DEBUG '%', sql;
-	EXECUTE sql INTO tpseg_max_size;
-
-	sql := 'select * from ' || quote_ident(c_trajectory_segtable_name) || ' where mpid = ' || c_trajectory.moid || 
-		' order by start_time';
-
-	IF( (c_start_time <= c_tpoint[tpoint_min_size].ts) AND (c_end_time >= c_tpoint[tpoint_max_size].ts)) THEN
-	-- 사용자 입력 시간과 tpoint[]의 시간이 contain되었을 때
-		FOR data IN EXECUTE sql LOOP
-			-- 읽어온 row에 사용자 입력 데이터로 변환되어야 할 데이터가 있을때
-			IF( ( data.start_time <= c_end_time ) AND ( data.end_time >= c_start_time ) ) THEN
-				IF( data.start_time <= c_start_time ) THEN
-					i := 1;
-					tp_seg := data.tpseg;
-					WHILE( i <= data.mpcount ) LOOP
-						-- 사용자가 입력한 start_time보다 저장된 데이터(tpseg)의 시간이 같거나 크다면
-						-- 현재의 앞부분까지가 보존해야 할 데이터이다.
-						IF( c_start_time > tp_seg[i].ts AND c_start_time <= tp_seg[i+1].ts) THEN
-							tpseg_temp1 := tp_seg[1:i];
-						END IF;
-						-- 사용자가 입력한 end_time보다 저장된 데이터(tpseg)의 시간이 더 크다면
-						-- 현재부터 보존해야 할 데이터이다.
-						IF( c_end_time < tp_seg[i].ts AND c_end_time >= tp_seg[i-1].ts) THEN
-							tpseg_temp2 := tp_seg[i:data.mpcount];
-							i := data.mpcount;
-						END IF;
-						i := i+1;
-					END LOOP;
-					-- temp3에 원본 데이터의 보존해야할 앞부분과 사용자가 입력한 데이터를 합해준다.
-
-					EXECUTE 'select array_cat($1, $2)'
-					INTO tpseg_temp3 USING tpseg_temp1, c_tpoint;
-
-					-- temp3에 원본 데이터의 보존해야할 데이터의 뒷부분을 넣어준다.
-					EXECUTE 'select array_cat( $1, $2)'
-					INTO tpseg_temp3 USING tpseg_temp3, tpseg_temp2;
-
-					EXECUTE 'select array_upper($1, 1)'
-					INTO tpseg_temp3_size USING tpseg_temp3;
-
-					new_tpseg := tpseg_temp3;
-					-- 새로운 tpseg의 크기가 tpseg_max_size보다 작거나 같다면 그냥 update(삽입) 해주면 된다.
-					IF( tpseg_temp3_size <= tpseg_max_size ) THEN
-						EXECUTE 'update ' || quote_ident(c_trajectory_segtable_name) ||
-							' set tpseg = $1, start_time = $2, end_time = $3, mpcount = $4 where mpid = $5 and segid = $6'
-						USING new_tpseg, new_tpseg[1].ts, new_tpseg[tpseg_temp3_size].ts, tpseg_temp3_size, data.mpid, data.segid;
-						-- rect 계산
-						EXECUTE 'select getBox2D($1)'
-						INTO new_rect USING new_tpseg;
-						-- rect 갱신
-						EXECUTE 'update ' || quote_ident(c_trajectory_segtable_name) ||
-							' set rect = $1 where mpid = $2 and segid = $3'
-						USING new_rect, data.mpid, data.segid;
-
-						
-						
-					-- 업데이트해야 할 tpseg가 tpseg_max_size보다 크다면
-					ELSE 
-						-- 우선 현재row에 데이터를 tpseg_max_size개만 삽입해준다.
-						new_tpseg := tpseg_temp3[1:tpseg_max_size];
-						EXECUTE 'update '  || quote_ident(c_trajectory_segtable_name) ||
-							' set tpseg = $1, start_time = $2, end_time = $3, mpcount = $4 where mpid = $5 and segid = $6'
-						USING new_tpseg, new_tpseg[1].ts, new_tpseg[tpseg_max_size].ts, tpseg_max_size, data.mpid, data.segid;
-
-						EXECUTE 'select getBox2D($1)'
-						INTO new_rect USING new_tpseg;
-						EXECUTE 'update ' || quote_ident(c_trajectory_segtable_name) ||
-							' set rect = $1 where mpid = $2 and segid = $3'
-						USING new_rect, data.mpid, data.segid;
-							
-						-- 현 row의 next_segid값을 가져와서 다음에 만들 row의 next_segid값을 준비한다.
-						EXECUTE 'select next_segid from ' || quote_ident(c_trajectory_segtable_name) ||
-								' where mpid = $1 and segid = $2'
-						INTO new_next_segid USING data.mpid, data.segid;
-						-- 현 row가 다음에 만들 row의 before_segid값이 된다.
-						new_before_segid = data.segid;
-						WHILE ( tpseg_temp3_size > tpseg_max_size ) LOOP
-							tpseg_temp3 := tpseg_temp3[tpseg_max_size + 1:tpseg_temp3_size];
-
-							-- new_tpseg에 최대 크기만큼 붙여준다(새로운 row에 삽입될 데이터이다.)
-							new_tpseg := tpseg_temp3[1:tpseg_max_size];
-
-							EXECUTE 'select array_upper($1, 1)'
-							INTO tpseg_temp3_size USING tpseg_temp3;
-
-							EXECUTE 'select array_upper($1, 1)'
-							INTO new_tpseg_size USING new_tpseg;
-
-							-- max_segid를 계산해야 한다. 							
-							EXECUTE 'select max(segid) from ' || quote_ident(c_trajectory_segtable_name) ||
-									' where mpid = $1'
-							INTO max_segid USING data.mpid;
-							
-							new_row_segid := max_segid+1;
-								
-							EXECUTE 'INSERT INTO ' || quote_ident(c_trajectory_segtable_name) || 
-									'(mpid, segid, next_segid, before_segid, mpcount, start_time, end_time, tpseg) ' || 
-									'values($1, $2, $3, $4, $5, $6, $7, $8)'
-							USING data.mpid, new_row_segid, new_next_segid, new_before_segid, new_tpseg_size, new_tpseg[1].ts, new_tpseg[new_tpseg_size].ts, new_tpseg;
-
-							EXECUTE 'select getBox2D($1)'
-							INTO new_rect USING new_tpseg;
-
-							EXECUTE 'UPDATE ' || quote_ident(c_trajectory_segtable_name) || 
-								' set rect = getBox2D($1) where mpid = $2 and segid = $3'
-							USING new_tpseg, data.mpid, new_row_segid;
-
-							-- 현재 row의 next_segid를 갱신
-							EXECUTE 'UPDATE ' || quote_ident(c_trajectory_segtable_name) || 
-								' set next_segid = $1 where mpid = $2 and segid = $3'
-							USING new_row_segid, data.mpid, new_before_segid;
-
-							-- 현재 row의 next_segid값을 가진 row의 before_segid값을 갱신
-							EXECUTE 'UPDATE ' || quote_ident(c_trajectory_segtable_name) || 
-								' set before_segid = $1 where mpid = $2 and segid = $3'
-							USING new_row_segid, data.mpid, new_next_segid;
-
-							new_before_segid := new_row_segid;
-
-
-						END LOOP;
-					END IF;
-				ELSIF( ( data.start_time > c_start_time ) AND ( data.end_time <= c_end_time ) ) THEN
-					-- row 삭제전 자신을 next_segid, before_segid로 가지고 있던 데이터들을 변경해주어야 한다.
-					EXECUTE 'update ' || quote_ident(c_trajectory_segtable_name) || 
-						' set next_segid = $1 where mpid = $2 and next_segid = $3'
-					USING data.next_segid, data.mpid, data.segid;
-
-					EXECUTE 'update ' || quote_ident(c_trajectory_segtable_name) || 
-						' set before_segid = $1 where mpid = $2 and before_segid = $3'
-					USING data.before_segid, data.mpid, data.segid;
-
-					-- row 삭제
-					EXECUTE 'DELETE FROM ' || quote_ident(c_trajectory_segtable_name) || 
-						' where mpid = $1 and segid = $2'
-					USING data.mpid, data.segid;
-
-				ELSIF( data.end_time > c_end_time ) THEN
-					-- row의 데이터 일부분 삭제
-					-- 변경하고자 하는 데이터의 끝부분이 현재 row의 일부분에 있다면 해당되는 데이터만 삭제를 해준다.
-					EXECUTE 'select remove(' || c_trajectory || ', TIMESTAMP ' || quote_literal(data.start_time) || ', TIMESTAMP ' || quote_literal(c_end_time) || ')';
-				ELSE
-
-				END IF;
-					
-			END IF;
-		END LOOP;
-		
-	ELSE
-		-- 사용자 입력시간과 tpoint[]의 시간이 contain 되지 않을때
-		RAISE NOTICE 'data time dose not match';
-	END IF;
-
-	RETURN c_trajectory;
-END
-$$
-LANGUAGE 'plpgsql';
-
-
---SLICE 함수 trajectory와 시작시간 끝시간을 입력해야 한다.
-CREATE OR REPLACE FUNCTION slice(trajectory, timestamp, timestamp) RETURNS tpoint[] AS
+CREATE OR REPLACE FUNCTION tj_slice(trajectory, timestamp, timestamp) RETURNS tpoint[] AS
 $$
 DECLARE
 	c_trajectory	alias for $1;
@@ -1082,12 +731,8 @@ BEGIN
 
 			WHILE( i <= data.mpcount ) LOOP
 				IF( tp_seg[i].ts >= start_time AND tp_seg[i].ts <= end_time ) THEN
-					IF( new_tpseg = NULL  ) THEN
-						new_tpseg := tp_seg[1:i];
-					ELSE
-						EXECUTE 'select array_append($1, $2)'
-						INTO new_tpseg USING new_tpseg, tp_seg[i];
-					END IF;
+					EXECUTE 'select array_append($1, $2)'
+					INTO new_tpseg USING new_tpseg, tp_seg[i];
 				END IF;
 				i := i+1;
 			END LOOP;
@@ -1100,8 +745,8 @@ $$
 LANGUAGE 'plpgsql';
 
 
--- SLICE 함수 trajectory와 4개의 좌표를 입력한다. 
-CREATE OR REPLACE FUNCTION slice(trajectory, double precision, double precision, double precision, double precision)
+-- TJ_SLICE 함수 trajectory와 4개의 좌표를 입력한다. 
+CREATE OR REPLACE FUNCTION tj_slice(trajectory, double precision, double precision, double precision, double precision)
   RETURNS tpoint[] AS
 $BODY$
 DECLARE
@@ -1175,8 +820,8 @@ $BODY$
 LANGUAGE 'plpgsql' VOLATILE
 COST 100;
 
--- SLICE 함수 trajectory와 geometry를 입력한다.
-CREATE OR REPLACE FUNCTION slice(trajectory, geometry)
+-- TJ_SLICE 함수 trajectory와 geometry를 입력한다.
+CREATE OR REPLACE FUNCTION tj_slice(trajectory, geometry)
   RETURNS tpoint[] AS
 $BODY$
 DECLARE
@@ -1242,6 +887,9 @@ END
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE
 COST 100;
+
+
+
 
 
 
